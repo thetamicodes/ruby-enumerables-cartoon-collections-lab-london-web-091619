a50145510eed16dd531c333a581d47b291427c07
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 13:13:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  overlap = []
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each { |item|  }
+  array.each { |item| overlap << item }
   end
 end
 
