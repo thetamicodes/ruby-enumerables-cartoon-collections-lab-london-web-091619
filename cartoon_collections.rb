cbf9851bab3eae1bd8b40d1e8e131b95e7342658
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 13:20:07 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def roll_call_dwarves(array)
   array.each_with_index { |item, index| puts "#{index.next}. #{item}" }
 end
@@ -7,15 +9,21 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.all? { |word| puts word.length > 4 }
+  array.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese(array)
+  overlap = []
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.any? { |item| cheese_types.include?(item) }
-  return item
-else
-  return nil 
+  array.each { |item| overlap << item if cheese_types.include?(item) }
+  if overlap.any?
+    return overlap[0]  
+  else
+    return nil
+  end
 end
+
+
+
 
 
