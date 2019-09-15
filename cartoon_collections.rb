c6d8cc2695c0c022e862f759054e1d45935fdaad
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 13:01:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,16 +11,14 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-cheese_types = ["cheddar", "gouda", "camembert"]
-result = array & cheese_types
-  final = result.each { |item| }
-    if final.any? == true  
-      return final[0]
-    else
-      return nil
-    end
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.any? { |item| cheese_types.include?(item) }
+    return item
+  else
+    return nil
   end
 end
+
 
 
 
