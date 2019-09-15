acb089312ed20f3b6e0b4bdbc5e528f6a613b9f9
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 12:24:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types)
+  if array.include?(item == cheese_types)
     return item
   else
     return nil
