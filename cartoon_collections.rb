0a94ab245ecc3ebde19f7fe46125c880b8b2e08c
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 12:05:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? { |word| word.length >= 4 }
+  array.any? { |word| word.length > 4 }
 end
 
 def find_the_cheese(array)
